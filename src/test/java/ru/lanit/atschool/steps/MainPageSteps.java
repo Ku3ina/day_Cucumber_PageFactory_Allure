fe607c6fb30904063e11482e250b5556c1343846
Ku3ina <62447555+Ku3ina@users.noreply.github.com>
2020-04-08 16:02:52 +0300

    add pageObjects

--- src/test/java/ru/lanit/atschool/steps/MainPageSteps.java
+++ src/test/java/ru/lanit/atschool/steps/MainPageSteps.java
@@ -3,42 +3,60 @@ package ru.lanit.atschool.steps;
 import io.cucumber.java.ru.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import ru.lanit.atschool.pages.BasePage;
+import ru.lanit.atschool.pages.FirstPage;
 import ru.lanit.atschool.pages.MainPage;
 import ru.lanit.atschool.webdriver.WebDriverManager;
 
-import java.util.HashMap;
 
-public class MainPageSteps {
-    WebDriver driver = WebDriverManager.getDriver();
+public class MainPageSteps  {
     MainPage mainPage = new MainPage();
+    FirstPage firstPage = new FirstPage(); //создали новый экземпляр класса
 
     @Пусть("открыт браузер и введен адрес \"(.*)\"$")
     public void openedBrowserAndEnteredUrl(String url) {
         mainPage.openPage(url);
-
-//        driver = WebDriverManager.getDriver();
-//        driver.get(url);
     }
 
-    @Пусть("пользователь вводит \"(.*)\"$")
-    public void userEnters(String arg0) {
-        driver = WebDriverManager.getDriver();
-        WebElement webElement = driver.findElement(By.name("q"));
+    @Тогда("пользователь переходит в раздел Пользователи")
+    public void goToUsers() {
+        firstPage.getUsersPage().click(); //используем метод класса
+
+        // String title = driver.getTitle();
+        //  Assert.assertEquals(title, "Top posters | Пользователи | Lanit education", "Тест не пройден");
     }
 
-    @И("заведен новый пользователь")
-    public void newUserRegistered() {
+    @Пусть("пользователь переходит в раздел Категории")
+    public void goToCategories() {
+        firstPage.getCategoriesPage().click();
+
+        // String title = driver.getTitle();
+        // Assert.assertEquals(title, "Категории | Lanit education", "Тест не пройден");
     }
 
-    @Тогда("пользователь вводит учетные данные")
-    public void userEnteredCredentials(HashMap<String, String> params) {
-        params.get("login");
-        params.get("password");
+    @И("пользователь ищет в системе \"(.*)\"$")
+    public void searchUser(String user) {
+        firstPage.getSearchPage().click();
+        firstPage.getSearchPageButton().sendKeys(user);
+        firstPage.getSearchPageAccessButton().click();
+
+        //   firstPage.getSearchPage().click();
+        //   driver.findElement(By.xpath("//*[@id='user-menu-mount']//ul/li[1]/input")).click();
+        //   driver.findElement(By.xpath("//*[@id='user-menu-mount']//ul/li[3]/a//div[2]/h5")).click();
+
     }
 
-    @И("вот")
-    public void vot() {
+    @Тогда("тест завершен")
+    public void testFinalPage() {
+        firstPage.getSearchPageAccessButton();
+     //    String title = driver.getTitle();
+     //     Assert.assertEquals(title, "Сообщений | Svetlana | Lanit education", "Тест не пройден");
 
     }
+
+
 }
+
+
+
