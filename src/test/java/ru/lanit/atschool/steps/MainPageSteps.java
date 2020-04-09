ac7273658798c2864a92d14528264c6e2a8f9861
Ku3ina <62447555+Ku3ina@users.noreply.github.com>
2020-04-09 16:38:14 +0300

    add 2 assert test

--- src/test/java/ru/lanit/atschool/steps/MainPageSteps.java
+++ src/test/java/ru/lanit/atschool/steps/MainPageSteps.java
@@ -16,6 +16,7 @@ import ru.lanit.atschool.webdriver.WebDriverManager;
 public class MainPageSteps {
     MainPage mainPage = new MainPage();
     Logger logger = Logger.getLogger(getClass());
+    WebDriver driver = WebDriverManager.getDriver();
 
 
     @Пусть("открыт браузер и введен адрес \"(.*)\"$")
@@ -26,12 +27,16 @@ public class MainPageSteps {
     @Тогда("пользователь переходит в раздел Пользователи")
     public void goToUsers() { // page factory
         mainPage.users.click();
+        String title = driver.getCurrentUrl();
+        Assert.assertEquals(title, "https://dev.n7lanit.ru/users/active-posters/", "Адрес страницы неверен");
         logger.info("Выполнен переход в раздел \"Пользователи\" ");
     }
 
     @Пусть("пользователь переходит в раздел Категории")
     public void goToCategories() {
         mainPage.categories.click();
+        String title = driver.getCurrentUrl();
+        Assert.assertEquals(title, "https://dev.n7lanit.ru/categories/", "Адрес страницы неверен");
         logger.info("Выполнен переход в раздел \"Категории\" ");
     }
 
@@ -40,7 +45,6 @@ public class MainPageSteps {
         mainPage.searchField.click();
         mainPage.searchBtn1.sendKeys(user);
         mainPage.searchBtn2.click();
-        WebDriver driver = WebDriverManager.getDriver();
         String title = driver.getCurrentUrl();
 //        Assert.assertEquals(title, "https://dev.n7lanit.ru/u/svetlana/14/poss/", "Тест не пройден"); // этот тест упадет
         Assert.assertEquals(title, "https://dev.n7lanit.ru/u/svetlana/14/posts/", "Адрес страницы неверен"); // этот выполнится
